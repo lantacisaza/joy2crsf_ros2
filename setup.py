1c95e3076aa515ff1f29809f784179c902f3ae5a
lantacisaza <azamat.abdikarimov.sakenuly@gmail.com>
2025-08-27 15:32:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-from setuptools import find_packages, setup
+# ~/ros2_ws/src/joy2crsf_ros2/setup.py
+from setuptools import setup
 
 package_name = 'crsf_joystick'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=['crsf_joystick'],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='lantacis',
-    maintainer_email='lantacis@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joystick_to_crsf = crsf_joystick.joystick_to_crsf:main',
+            'joystick_to_crsf = crsf_joystick.joystick_to_crsf:main',  # <- matches your file
+            # (optional) also export 'joy_to_crsf' if you add that file later
         ],
     },
 )
